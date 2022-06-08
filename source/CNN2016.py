0a77a38c47a9ed8251b103daeeea8591984b54d7
alvinzang98 <wenshuoalvin@gmail.com>
2022-06-08 09:50:51 -0700

    codes

---
+++ source/CNN2016.py
@@ -0,0 +1,170 @@
+#%% import
+import numpy as np
+import pickle
+from sklearn.metrics import confusion_matrix
+from keras import Sequential
+from keras.layers import ZeroPadding2D, Conv2D, Dropout, Flatten, Dense
+from mlxtend.plotting import plot_confusion_matrix
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+#%% pre-process data
+
+f = open("RML2016.10a_dict.pkl", 'rb')
+Xd = pickle.load(f, encoding='latin1')
+snrs, mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1, 0])
+X = []
+lbl = []
+for mod in mods:
+    for snr in snrs:
+        X.append(Xd[(mod, snr)])
+        for i in range(Xd[(mod, snr)].shape[0]):  lbl.append((mod, snr))
+X = np.vstack(X)
+np.random.seed(2016)
+n_examples = X.shape[0]
+n_train = n_examples * 0.5
+train_idx = np.random.choice(range(0, int(n_examples)), size=int(n_train), replace=False)
+test_idx = list(set(range(0, n_examples)) - set(train_idx))
+X_train = X[train_idx][:,:,:,np.newaxis]
+X_test = X[test_idx][:,:,:,np.newaxis]
+N, H, W,_ = X_train.shape
+
+
+def to_onehot(yy):
+    yy = list(yy)
+    yy1 = np.zeros([len(yy), max(yy) + 1])
+    yy1[np.arange(len(yy)), yy] = 1
+    return yy1
+
+
+Y_train = to_onehot(map(lambda x: mods.index(lbl[x][0]), train_idx))
+Y_test = to_onehot(map(lambda x: mods.index(lbl[x][0]), test_idx))
+in_shp = list(X_train.shape[1:])
+print(X_train.shape, in_shp)
+classes = mods  # modulations(11 classes)
+
+#%% CNN model
+
+
+dr = 0.4  # dropout rate (%)
+model = Sequential(name='CNN_Architecture')
+model.add(ZeroPadding2D((0, 2), data_format='channels_last'))
+model.add(Conv2D(256, (1, 3), activation='relu', data_format='channels_last', input_shape=(H, W, 1), name='conv1'))
+model.add(Dropout(dr))
+model.add(ZeroPadding2D((0, 2), data_format='channels_last'))
+model.add(Conv2D(80, (2, 3), activation='relu', data_format='channels_last'))
+model.add(Dropout(dr))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(dr))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(len(classes), activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.build(input_shape=(None, H, W, 1))
+model.summary()
+
+"""
+training
+"""
+
+nb_epoch = 50
+batch_size = 1024
+filepath = 'baseline_simpleCNN_dr=0.5.h5'
+history = model.fit(X_train,
+                    Y_train,
+                    batch_size=batch_size,
+                    epochs=nb_epoch,
+                    verbose=2,
+                    validation_data=(X_test, Y_test),
+                    callbacks=[
+                        keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True,
+                                                        mode='auto'),
+                        keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+                    ])
+# we re-load the best weights once training is finished
+model.load_weights(filepath)
+score = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size)
+print(score)
+
+
+#%% show accuract & loss curve
+
+
+plt.figure()
+plt.title('Training performance')
+plt.plot(history.epoch, history.history['accuracy'], label='train_acc')
+plt.plot(history.epoch, history.history['val_accuracy'], label='val_acc')
+plt.legend()
+
+plt.figure()
+plt.title('Training performance')
+plt.plot(history.epoch, history.history['loss'], label='train_loss')
+plt.plot(history.epoch, history.history['val_loss'], label='val_loss')
+plt.legend()
+
+
+#%% plot confusion matrix
+
+
+model.load_weights("baseline_simpleCNN_dr=0.5.h5")
+batch = 1024
+y_pred = model.predict(X_test, batch)
+score = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch)
+print(score)
+predict_x = model.predict(X_test, batch)
+y_pred_label = np.argmax(predict_x, axis=1)
+y_test_label = np.argmax(Y_test, axis=1)
+cm = confusion_matrix(y_test_label, y_pred_label, normalize='true')
+cm = confusion_matrix(y_test_label, y_pred_label)
+fg, ax = plot_confusion_matrix(cm, colorbar=True,
+                               show_absolute=False,
+                               show_normed=True)
+ax.set_xticks(np.arange(len(classes)))
+ax.set_xticklabels(classes, rotation=45)
+ax.set_title("CNN Confusion Matrix")
+ax.set_yticks(np.arange(len(classes)))
+ax.set_yticklabels(classes)
+fg.set_size_inches(16.5, 8.5, forward=True)
+plt.show()
+
+"""
+plot confusion matrix vs SNRs
+"""
+
+acc = {}
+for snr in snrs:
+    # extract classes @ SNR
+    test_SNRs = list(map(lambda x: lbl[x][1], test_idx))
+    test_X_i = X_test[np.where(np.array(test_SNRs) == snr)]
+    test_Y_i = Y_test[np.where(np.array(test_SNRs) == snr)]
+
+    # estimate classes
+    test_Y_i_hat = model.predict(test_X_i,batch_size=128)
+    conf = np.zeros([len(classes), len(classes)])
+    confnorm = np.zeros([len(classes), len(classes)])
+    for i in range(0, test_X_i.shape[0]):
+        j = list(test_Y_i[i, :]).index(1)
+        k = int(np.argmax(test_Y_i_hat[i, :]))
+        conf[j, k] = conf[j, k] + 1
+    for i in range(0, len(classes)):
+        confnorm[i, :] = conf[i, :] / np.sum(conf[i, :])
+    plt.figure()
+    plot_confusion_matrix(confnorm, class_names=classes)
+    plt.title("ConvNet Confusion Matrix(SNR=%d)" % (snr))
+    cor = np.sum(np.diag(conf))
+    ncor = np.sum(conf) - cor
+    print("Overall Accuracy: ", cor / (cor + ncor))
+    acc[snr] = 1.0 * cor / (cor + ncor)
+
+"""
+print accuracy curve vs SNRs
+"""
+
+# Plot accuracy curve
+plt.plot(snrs, list(map(lambda x: acc[x], snrs)))
+plt.xlabel("Signal to Noise Ratio")
+plt.ylabel("Classification Accuracy")
+plt.title("CNN2 Classification Accuracy on RadioML 2016.10 Alpha")
---
+++ source/CNN2018.py
@@ -0,0 +1,248 @@
+#%% import 
+import tensorflow as tf
+import tensorflow.compat.v1.keras.backend as K
+import DataGenerator
+print(tf.keras.__version__)
+import numpy as np
+import h5py
+import os, random
+import numpy as np
+from tensorflow.keras.layers import Input, Reshape, ZeroPadding2D, Conv2D, Dropout, Flatten, Dense, Activation, \
+    MaxPool2D, AlphaDropout
+from tensorflow.keras import layers
+import tensorflow.keras.models as Model
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+data_path = 'data'
+#%%
+for i in range(0, 2):  # 24 sub-datasets hardcode
+    ########open each sub-dataset file#######
+    filename = os.path.join(data_path,'ExtractDataset','part') + str(i) + '.h5'
+    print(filename)
+    f = h5py.File(filename, 'r')
+    ########read data#######
+    X_data = f['X'][:]
+    Y_data = f['Y'][:]
+    Z_data = f['Z'][:]
+    f.close()
+    #########分割训练集和测试集#########
+    # 每读取到一个数据文件就直接分割为训练集和测试集，防止爆内存
+    n_examples = X_data.shape[0]
+    n_train = int(n_examples * 0.7)  # 70%训练样本
+    train_idx = np.random.choice(range(0, n_examples), size=n_train, replace=False)  # 随机选取训练样本下标
+    test_idx = list(set(range(0, n_examples)) - set(train_idx))  # 测试样本下标
+    if i == 0:
+        X_train = X_data[train_idx]
+        Y_train = Y_data[train_idx]
+        Z_train = Z_data[train_idx]
+        X_test = X_data[test_idx]
+        Y_test = Y_data[test_idx]
+        Z_test = Z_data[test_idx]
+    else:
+        X_train = np.vstack((X_train, X_data[train_idx]))
+        Y_train = np.vstack((Y_train, Y_data[train_idx]))
+        Z_train = np.vstack((Z_train, Z_data[train_idx]))
+        X_test = np.vstack((X_test, X_data[test_idx]))
+        Y_test = np.vstack((Y_test, Y_data[test_idx]))
+        Z_test = np.vstack((Z_test, Z_data[test_idx]))
+
+print('Training set X Dimension:', X_train.shape)
+print('Training set Y Dimension:', Y_train.shape)
+print('Training set Z Dimension:', Z_train.shape)
+print('Test set X Dimension:', X_test.shape)
+print('Test set Y Dimension:', Y_test.shape)
+print('Test set Z Dimension:', Z_test.shape)
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+print(tf.test.gpu_device_name())
+classes = ['32PSK',
+           '16APSK',
+           '32QAM',
+           'FM',
+           'GMSK',
+           '32APSK',
+           'OQPSK',
+           '8ASK',
+           'BPSK',
+           '8PSK',
+           'AM-SSB-SC',
+           '4ASK',
+           '16PSK',
+           '64APSK',
+           '128QAM',
+           '128APSK',
+           'AM-DSB-SC',
+           'AM-SSB-WC',
+           '64QAM',
+           'QPSK',
+           '256QAM',
+           'AM-DSB-WC',
+           'OOK',
+           '16QAM']
+
+X_test = X_test.reshape(-1, 2, 1024, 1)
+X_train = X_train.reshape(-1, 2, 1024, 1)
+data_format = 'channels_last'
+
+#%%resnet building
+
+def residual_stack(Xm, kennel_size, Seq, pool_size, if_max):
+    # 1*1 Conv Linear original filtersize 32
+    Xm = Conv2D(32, (1, 1), padding='same', name=Seq + "_conv1", kernel_initializer='glorot_normal',
+                data_format=data_format)(Xm)
+    # Residual Unit 1
+    Xm_shortcut = Xm
+    Xm = Conv2D(32, kennel_size, padding='same', activation="relu", name=Seq + "_conv2",
+                kernel_initializer='glorot_normal', data_format=data_format)(Xm)
+    Xm = Conv2D(32, kennel_size, padding='same', name=Seq + "_conv3", kernel_initializer='glorot_normal',
+                data_format=data_format)(Xm)
+    Xm = layers.add([Xm, Xm_shortcut])
+    Xm = Activation("relu")(Xm)
+    # Residual Unit 2
+    Xm_shortcut = Xm
+    Xm = Conv2D(32, kennel_size, padding='same', activation="relu", name=Seq + "_conv4",
+                kernel_initializer='glorot_normal', data_format=data_format)(Xm)
+    Xm = Conv2D(32, kennel_size, padding='same', name=Seq + "_conv5", kernel_initializer='glorot_normal',
+                data_format=data_format)(Xm)
+    Xm = layers.add([Xm, Xm_shortcut])
+    Xm = Activation("relu")(Xm)
+    # MaxPooling
+    if (if_max):
+        Xm = MaxPool2D(pool_size=pool_size, strides=pool_size, padding='valid', data_format=data_format)(Xm)
+    return Xm
+
+
+in_shp = X_train.shape[1:]  # [1024,2]
+print(in_shp)
+# input layer
+Xm_input = Input(in_shp, name='input')
+# Xm = Reshape([1,512,4], input_shape=in_shp)(Xm_input)
+
+
+# Residual Srack
+
+Xm = residual_stack(Xm_input, kennel_size=(3, 2), Seq="ReStk0", pool_size=(2, 2),
+                    if_max=False)
+X = MaxPool2D(pool_size=(2, 2), strides=(2, 1), padding='valid', data_format=data_format)(Xm)
+Xm = residual_stack(Xm, kennel_size=(3, 2), Seq="ReStk1", pool_size=(1, 2), if_max=True)  # shape:(256,1,32)
+Xm = residual_stack(Xm, kennel_size=(3, 2), Seq="ReStk2", pool_size=(1, 2), if_max=True)  # shape:(128,1,32)
+Xm = residual_stack(Xm, kennel_size=(3, 2), Seq="ReStk3", pool_size=(1, 2), if_max=True)  # shape:(64,1,32)
+Xm = residual_stack(Xm, kennel_size=(3, 2), Seq="ReStk4", pool_size=(1, 2), if_max=True)  # shape:(32,1,32)
+Xm = residual_stack(Xm, kennel_size=(3, 2), Seq="ReStk5", pool_size=(1, 2), if_max=True)  # shape:(16,1,32)
+
+Xm = Flatten(data_format=data_format, name='flat')(Xm)
+Xm = Dense(128, activation='relu', kernel_initializer='glorot_normal', name="dense1")(Xm)
+Xm = AlphaDropout(0.3)(Xm)
+# Full Con 2
+Xm = Dense(len(classes), kernel_initializer='glorot_normal', name="dense2")(Xm)
+Xm = AlphaDropout(0.3)(Xm)
+# SoftMax
+Xm = Activation('softmax', name='activate')(Xm)
+# Create Model
+model = Model.Model(inputs=Xm_input, outputs=Xm)
+adam = tf.keras.optimizers.Adam(lr=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=adam)
+model.summary()
+
+#%%
+filepath = 'ResNet2018.h5'
+history = model.fit(X_train,
+                    Y_train,
+                    # batch_size=1000,
+                    batch_size=1000,  # already changed to 10, original one is 1000
+                    epochs=10,  # changed to 10, original one is 100
+                    verbose=2,
+                    validation_data=(X_test, Y_test),
+                    # validation_split = 0.3,
+                    callbacks=[
+                        tf.keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True,
+                                                           mode='auto'),
+                        tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
+                    ])
+
+#%% plot loss curve
+
+print('train finish')
+val_loss_list = history.history['val_loss']
+loss_list = history.history['loss']
+plt.plot(range(len(loss_list)), val_loss_list)
+plt.plot(range(len(loss_list)), loss_list)
+plt.show()
+
+model.load_weights(filepath)
+"""
+plot confusion matrix
+
+"""
+
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# Plot confusion matrix
+batch_size = 1024
+test_Y_hat = model.predict(X_test, batch_size=1024)
+conf = np.zeros([len(classes), len(classes)])
+confnorm = np.zeros([len(classes), len(classes)])
+for i in range(0, X_test.shape[0]):
+    j = list(Y_test[i, :]).index(1)
+    k = int(np.argmax(test_Y_hat[i, :]))
+    conf[j, k] = conf[j, k] + 1
+for i in range(0, len(classes)):
+    confnorm[i, :] = conf[i, :] / np.sum(conf[i, :])
+plot_confusion_matrix(confnorm, labels=classes)
+
+"""
+analysis vs 24 SNRs
+"""
+for i in range(len(confnorm)):
+    print(classes[i], confnorm[i, i])
+
+acc = {}
+Z_test = Z_test.reshape((len(Z_test)))
+SNRs = np.unique(Z_test)
+for snr in SNRs:
+    X_test_snr = X_test[Z_test == snr]
+    Y_test_snr = Y_test[Z_test == snr]
+
+    pre_Y_test = model.predict(X_test_snr)
+    conf = np.zeros([len(classes), len(classes)])
+    confnorm = np.zeros([len(classes), len(classes)])
+    for i in range(0, X_test_snr.shape[0]):  # 该信噪比下测试数据量
+        j = list(Y_test_snr[i, :]).index(1)  # 正确类别下标
+        j = classes.index(classes[j])
+        k = int(np.argmax(pre_Y_test[i, :]))  # 预测类别下标
+        k = classes.index(classes[k])
+        conf[j, k] = conf[j, k] + 1
+    for i in range(0, len(classes)):
+        confnorm[i, :] = conf[i, :] / np.sum(conf[i, :])
+
+    plt.figure()
+    plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)" % (snr))
+
+    cor = np.sum(np.diag(conf))
+    ncor = np.sum(conf) - cor
+    print("Overall Accuracy %s: " % snr, cor / (cor + ncor))
+    acc[snr] = 1.0 * cor / (cor + ncor)
+
+"""
+accuracy vs 24 snrs
+"""
+plt.plot(list(acc.keys()), list(acc.values()))
+plt.ylabel('ACC')
+plt.xlabel('SNR')
+plt.show()
+
+# %%
